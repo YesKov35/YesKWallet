294dd18ea4660cc207a781e7a48d51132f8f30ca
nik35 <wr35nik@gmail.com>
2020-02-21 00:11:09 +0200

    add text utils

--- app/src/main/java/com/yeskov35/yeskwallet/MainActivity.kt
+++ app/src/main/java/com/yeskov35/yeskwallet/MainActivity.kt
@@ -2,11 +2,15 @@ package com.yeskov35.yeskwallet
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.yeskov35.yeskwallet.utils.TextUtils
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        total_wallet.text = TextUtils.priceFormat(100000100)
     }
 }
